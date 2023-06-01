ccc68e3b2c4b1faf8b82300fe6e13424fce5047a
kshiohara <k.shiohara.cingroup2012@gmail.com>
2023-06-01 15:26:14 +0900

    first commit

---
+++ 01.値と型と変数/lesson3.php
@@ -0,0 +1,19 @@
+﻿<?php
+// 連想配列のkey
+// name, age, genderに
+// 山田,  20,  女性
+// という値を格納し、
+
+// 山田
+// 20歳・女性
+
+// という形で出力してください。
+
+$info = [
+  'name' => '山田',
+  'age' => 20,
+  'gender' => '女性',
+];
+
+echo $info['name'] . PHP_EOL;
+echo $info['age'] . "歳・" .  $info['gender'] . PHP_EOL;
