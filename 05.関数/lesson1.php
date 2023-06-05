a5a2cec19b570ee9caab64521d6c9211db37da63
kshiohara <k.shiohara.cingroup2012@gmail.com>
2023-06-05 13:02:48 +0900

    8.関数

---
+++ 05.関数/lesson1.php
@@ -0,0 +1,12 @@
+﻿<?php
+/**
+  * 引数として数値を渡すと3倍にして返す関数を作ってください。
+  * @param int $number  入力する値
+*/
+
+function convertThreeTimes($number) {
+  return $number * 3;
+}
+
+echo convertThreeTimes(5);
+// 15
